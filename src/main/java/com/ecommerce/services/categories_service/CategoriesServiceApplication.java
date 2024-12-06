d7b3a720db355bcd10abbcc5af35a9660c574e50
rkommarraju <rohitkommarraju@gmail.com>
2024-12-06 08:28:51 -0500

    setting up project structure

---
+++ src/main/java/com/ecommerce/services/categories_service/CategoriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.services.categories_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoriesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/services/categories_service/CategoriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.services.categories_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
